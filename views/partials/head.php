ccda0d258dcfa19fe4d540ff8f4d60d6ee728c61
maximebergerard <maxime.bergerard@hetic.net>
2019-04-10 13:29:04 +0200

    :bug: routing fixed

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,6 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Arbutus+Slab|Montserrat:300,700|Source+Sans+Pro:300,400,600,600i,700" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../public/assets/styles/style.css">
+    <link rel="stylesheet" type="text/css" href="./public/assets/styles/style.css">
     <title><?= !empty($title) ? $title : '' ?></title>
 </head>
