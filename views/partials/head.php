8a3197dd7e8da73a3de476fc23bad0c6ce73c895
SaucySpray <alexandrechabeau.pro@gmail.com>
2019-04-11 22:32:55 +0200

    :lipstick: favicon

--- views/partials/head.php
+++ views/partials/head.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="./public/assets/img/favicon.png">
     <link href="https://fonts.googleapis.com/css?family=Arbutus+Slab|Montserrat:300,700|Source+Sans+Pro:300,400,600,600i,700" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./public/assets/styles/style.css">
     <title><?= !empty($title) ? $title : '' ?></title>
