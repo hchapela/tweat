c6a93d7b2dfa217cde81d3af426d83617866135e
SaucySpray <alexandrechabeau.pro@gmail.com>
2019-04-11 22:50:12 +0200

    :lipstick: 404 floating shadows

--- views/pages/404.php
+++ views/pages/404.php
@@ -2,9 +2,18 @@
 <?php include (VIEWS.'partials/header.php') ?>
 <div class="error404">
     <div class="error-image">
-        <div class="error-svg error4-1"></div>
-        <div class="error-svg error0"></div>
-        <div class="error-svg error4-2"></div>
+        <div class="error-image-container error-image-container-1">
+            <div class="error-svg error4-1"></div>
+            <div class="error-shadow"></div>
+        </div>
+        <div class="error-image-container error-image-container-2">
+            <div class="error-svg error0"></div>
+            <div class="error-shadow"></div>
+        </div>
+        <div class="error-image-container error-image-container-3">
+            <div class="error-svg error4-2"></div>
+            <div class="error-shadow"></div>
+        </div>
     </div>
     <div class="error-message">
         <div class="category azul">OoOps <span class="limon">...</span></div>
