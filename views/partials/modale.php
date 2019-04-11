5c26af30d2c2330ed3b1a3784287f002ecafc5b1
SaucySpray <alexandrechabeau.pro@gmail.com>
2019-04-11 18:45:51 +0200

    :wheelchair: Twitter cotoreps can now share our website !

--- views/partials/modale.php
+++ views/partials/modale.php
@@ -5,9 +5,9 @@
             <div class="modale-share-exit icon-exit"></div>
         </div>
         <div class="modale-share-message">
-            <p class="modale-share-example paragraph azul">Hey, I just checked my today’s perfect mood meal !<br>Check my results and try out yours !<br><span class="limon">http://tweat.io/</span></p>
+            <p class="modale-share-example paragraph azul"><span class="limon">@tweatDaily</span> I just discovered my today perfect meal match with !<br>Find out yours at <span class="limon">https://tweat.space/</span> !</p>
         </div>
-        <a href="https://twitter.com/intent/tweet?screen_name=tweatDaily&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">Tweet to @tweatDaily</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <a href="https://twitter.com/intent/tweet?screen_name=tweatDaily&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-size="large" data-text="I just discovered my today perfect meal match ! Find out yours at https://tweat.space/ !" data-related="tweatDaily" data-lang="en" data-show-count="false">Tweet to @tweatDaily</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         <form action=""></form>
     </div>
 </div>
